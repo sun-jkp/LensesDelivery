acc8d094a6ee3f0020cd7145d585cbaacc153962
sun-jkp <jakkapan.wonglam@transitions.com>
2024-03-21 13:24:54 +0700

    Remove used state

--- app/src/main/java/com/transition/lensesdelivery/presentation/delivery_confirm/DeliveryState.kt
+++ app/src/main/java/com/transition/lensesdelivery/presentation/delivery_confirm/DeliveryState.kt
@@ -1,20 +1,14 @@
 package com.transition.lensesdelivery.presentation.delivery_confirm
 
 import com.transition.lensesdelivery.domain.model.Queue
-import com.transition.lensesdelivery.domain.model.QueueDetail
 
 data class DeliveryState(
     val queue: Queue? = null,
-    val isLoading: Boolean = false,
-    val isRefreshing: Boolean = false,
-    val searchQuery: String = "",
     val isNavigate: Boolean = false,
     val isPlayMusic: Boolean = false,
     val isConnected: Boolean = false,
     val isRosConnected: Boolean = false,
-    val message: String = "",
     val robotMsg: String = "Initializing",
-    val queueDetail: QueueDetail? = null,
     val buttonState: MutableList<Boolean> = mutableListOf(
         false,
         false,
